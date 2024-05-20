452927efc2503b6d963f2c8aa7b4702566bb7ca3
Wenrui Huang <rijuyuezhuhwr@gmail.com>
2024-05-20 16:08:51 +0800

    style(*): use clang-format to format

--- 03_file-transfer/rdma-file-transfer/common.h
+++ 03_file-transfer/rdma-file-transfer/common.h
@@ -2,14 +2,22 @@
 #define RDMA_COMMON_H
 
 #include <netdb.h>
+#include <rdma/rdma_cma.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <rdma/rdma_cma.h>
 
-#define TEST_NZ(x) do { if ( (x)) rc_die("error: " #x " failed (returned non-zero)." ); } while (0)
-#define TEST_Z(x)  do { if (!(x)) rc_die("error: " #x " failed (returned zero/null)."); } while (0)
+#define TEST_NZ(x)                                                             \
+    do {                                                                       \
+        if ((x))                                                               \
+            rc_die("error: " #x " failed (returned non-zero).");               \
+    } while (0)
+#define TEST_Z(x)                                                              \
+    do {                                                                       \
+        if (!(x))                                                              \
+            rc_die("error: " #x " failed (returned zero/null).");              \
+    } while (0)
 
 typedef void (*pre_conn_cb_fn)(struct rdma_cm_id *id);
 typedef void (*connect_cb_fn)(struct rdma_cm_id *id);
@@ -20,7 +28,7 @@ void rc_init(pre_conn_cb_fn, connect_cb_fn, completion_cb_fn, disconnect_cb_fn);
 void rc_client_loop(const char *host, const char *port, void *context);
 void rc_disconnect(struct rdma_cm_id *id);
 void rc_die(const char *message);
-struct ibv_pd * rc_get_pd();
+struct ibv_pd *rc_get_pd();
 void rc_server_loop(const char *port);
 
 #endif
--- 03_file-transfer/rdma-file-transfer/messages.h
+++ 03_file-transfer/rdma-file-transfer/messages.h
@@ -4,26 +4,17 @@
 const char *DEFAULT_PORT = "12345";
 const size_t BUFFER_SIZE = 10 * 1024 * 1024;
 
-enum message_id
-{
-  MSG_INVALID = 0,
-  MSG_MR,
-  MSG_READY,
-  MSG_DONE
-};
+enum message_id { MSG_INVALID = 0, MSG_MR, MSG_READY, MSG_DONE };
 
-struct message
-{
-  int id;
+struct message {
+    int id;
 
-  union
-  {
-    struct
-    {
-      uint64_t addr;
-      uint32_t rkey;
-    } mr;
-  } data;
+    union {
+        struct {
+            uint64_t addr;
+            uint32_t rkey;
+        } mr;
+    } data;
 };
 
 #endif
